3fdec5bb8a1a8abdd8409837c1953cbf94800276
Shahriar Alvi <alam.shahriaralvi@gmail.com>
2023-02-16 13:38:29 +0600

    Padding, Margin and TextStyle

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(home: Home()));
+void main() => runApp(const MaterialApp(
+  home: Home()
+));
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,20 +14,25 @@ class Home extends StatelessWidget {
         title: const Text('my first app'),
         centerTitle: true,
         backgroundColor: Colors.amber[300],
+
       ),
-      body: Padding(
-        //color and margin can not be used
-        padding: EdgeInsets.all(100.0),
-        child: Text('hello',
-            style: TextStyle(
-              color: Colors.green,
-            )),
-      ),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(1, 10, 20, 30),
+        margin: EdgeInsets.all(30),
+        color: Colors.grey[300],
+        child: Text('hi',
+        style: TextStyle(
+          color: Colors.red,
+        ))
+      ) ,
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {  },
         child: Text('click'),
         backgroundColor: Colors.amber[300],
       ),
     );
   }
 }
+
+
+
