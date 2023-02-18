7568ba7a357bda6dbec22231b434d6566f5c4d65
Shahriar Alvi <alam.shahriaralvi@gmail.com>
2023-02-18 22:08:04 +0600

    creating an ID by myself

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MaterialApp(home: Home()));
@@ -8,48 +10,92 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: const Text('my first app'),
+        title: const Text('ID Card'),
         centerTitle: true,
         backgroundColor: Colors.amber[300],
       ),
-      body: Row(
+      body:
+      Column(
         children: [
           Expanded(
-            flex: 10,
-              child: Image.asset('assets/1672921962063.jpg')),
-          Expanded(
-            flex: 3,
+            flex: 6,
             child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.cyan,
-              child: Text('one'),
-            ),
-          ),
-
-          Expanded(
-            flex: 10,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.pink,
-              child: Text('Two'),),
+              //alignment: Alignment.centerLeft,
+                child: Image.asset('assets/1672921962063.jpg', )),
           ),
 
           Expanded(
             flex: 1,
             child: Container(
+              padding: EdgeInsets.fromLTRB(2, 2, 2, 0),
+              //color: Colors.blue,
+              alignment: Alignment.centerLeft,
+              child: Text('Name',
+                style: TextStyle(
+                  color: Colors.white60,
+                  fontSize: 17,
+                ),
+              ),
+            ),
+          ),
 
-              padding: EdgeInsets.all(30.0),
+          Expanded(
+            flex: 2,
+            child: Container(
+              padding: EdgeInsets.fromLTRB(2, 0, 2, 6),
+              //color: Colors.cyan,
+              alignment: Alignment.centerLeft,
+              child: Text('Sherlock Holmes',
+              style: TextStyle(
               color: Colors.amber,
-              child: Text('three'),
+                fontSize: 30,
+              ),
+              ),
+            ),
+          ),
+
+          Expanded(
+            flex: 1,
+            child: Container(
+                padding: EdgeInsets.fromLTRB(2, 2, 2, 6),
+                //color: Colors.green,
+                alignment: Alignment.centerLeft,
+                child: Text('Address',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ), )),
+          ),
+
+          Expanded(
+
+            flex: 3,
+            child: Container(
+                padding: EdgeInsets.fromLTRB(2, 2, 2, 2),
+                color: Colors.white,
+                alignment: Alignment.centerLeft,
+                child: Text('221/B Baker Street',
+                  style: TextStyle(
+                    color: Colors.yellow,
+                    fontSize: 40,
+                  ),)),
+          ),
+
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 250),
+            child: Row(
+              children: [
+                Icon(Icons.mail,
+                  color: Colors.white,),
+                Text('sherlocked@gmail..com',
+                    style: TextStyle(
+                    color: Colors.white,
+                ),)
+              ],
             ),
           ),
         ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('click'),
-        backgroundColor: Colors.amber[300],
       ),
     );
   }
